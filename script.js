b8172cb4818c9da4f7553ae04acbfb11720aeecf
Firat Yedibela <yedibelafirat0@gmail.com>
2024-01-31 01:04:45 +0300

    Implement password validation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+password.addEventListener('input', (event) => {
+  validatePasswords();
+});
+
+confirmPassword.addEventListener('input', (event) => {
+  validatePasswords();
+});
+
+function validatePasswords() {
+  console.log(password.value);
+  console.log(confirmPassword.value);
+  // If passwords match, remove error class else add error class
+  if (password.value === confirmPassword.value) {
+    password.classList.remove('error');
+    confirmPassword.classList.remove('error');
+  }
+  else {
+    password.classList.add('error');
+    confirmPassword.classList.add('error');
+  }
+}
+
